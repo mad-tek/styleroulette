88e0a28ac3556b19d599bf26bbc94106025062db
tekkenator <bboytek@gmail.com>
2015-03-10 21:17:30 +0000

    Update about and styles

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -7,13 +7,5 @@ class VideosController < ApplicationController
   end
 
   def create
-    @filters = Filter.new(params[:filters])
-      if @filters.save
-        flash[:success] = "Category changed to #{Filter.last.name}!"
-        redirect_to root_url
-      else
-        flash[:error] = "Did not change to #{@stylefilter}!"
-        redirect_to root_url
-      end
   end
 end
